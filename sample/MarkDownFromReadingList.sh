d3604a8ab6e701351acc07022085567374a604a1
Ming Yi Chen <ming@mitake.com.tw>
2016-11-05 02:07:17 +0800

    gb2312 encode

--- sample/MarkDownFromReadingList.sh
+++ sample/MarkDownFromReadingList.sh
@@ -2,16 +2,30 @@
 ./readinglisturls.py > ReadingList.txt
 
 inputfile="ReadingList.txt"
-outputfile="OutputReadingList.md"
+
+DATE=$(date +%Y%m%d)
+
+outputfile="OutputReadingList_$DATE.md"
+oriOutput="tmp.md"  
+encOutput="encode.md"
 printf "" > $outputfile
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	printf "."
 	printf "* [" >> $outputfile;
-	wget -qO- $line | perl -l -0777 -ne 'printf $1 if /<title.*?>\s*(.*?)\s*<\/title/si' >> $outputfile
-	printf "]($line)" >> $outputfile;
+	wget -qO- $line | perl -l -0777 -ne 'printf $1 if /<title.*?>\s*(.*?)\s*<\/title/si' > $oriOutput
+	
+	#for gb2312 	
+	if iconv -f cp936 -t utf-8 $oriOutput > $encOutput ; then 		
+		cat $encOutput >> $outputfile
+	else 
+		cat $oriOutput >> $outputfile;
+	fi
+	printf "](%s)" $line >> $outputfile;
 	printf "\n" >> $outputfile;
 done < $inputfile
 printf "Complete!!"
+
+
 
 rm ReadingList.txt
 
